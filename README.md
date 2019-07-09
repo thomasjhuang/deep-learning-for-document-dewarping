1bec03180d18ae0767e1750b33ece4c412bc65fe
Thomas Huang <thomashuang63@gmail.com>
2019-07-09 21:48:14 +0000

    Completed working model

--- README.md
+++ README.md
@@ -1,126 +1,144 @@
-# Insight_Project_Framework
-Framework for machine learning projects at Insight Data Science.
+<img src='imgs/teaser_720.gif' align="right" width=360>
 
-## Motivation for this project format:
-- **Insight_Project_Framework** : Put all source code for production within structured directory
-- **tests** : Put all source code for testing in an easy to find location
-- **configs** : Enable modification of all preset variables within single directory (consisting of one or many config files for separate tasks)
-- **data** : Include example a small amount of data in the Github repository so tests can be run to validate installation
-- **build** : Include scripts that automate building of a standalone environment
-- **static** : Any images or content to include in the README or web framework if part of the pipeline
+<br><br><br><br>
 
-## Setup
-Clone repository and update python path
+# pix2pixHD
+### [Project](https://tcwang0509.github.io/pix2pixHD/) | [Youtube](https://youtu.be/3AIpPlzM_qs) | [Paper](https://arxiv.org/pdf/1711.11585.pdf) <br>
+Pytorch implementation of our method for high-resolution (e.g. 2048x1024) photorealistic image-to-image translation. It can be used for turning semantic label maps into photo-realistic images or synthesizing portraits from face label maps. <br><br>
+[High-Resolution Image Synthesis and Semantic Manipulation with Conditional GANs](https://tcwang0509.github.io/pix2pixHD/)  
+ [Ting-Chun Wang](https://tcwang0509.github.io/)<sup>1</sup>, [Ming-Yu Liu](http://mingyuliu.net/)<sup>1</sup>, [Jun-Yan Zhu](http://people.eecs.berkeley.edu/~junyanz/)<sup>2</sup>, Andrew Tao<sup>1</sup>, [Jan Kautz](http://jankautz.com/)<sup>1</sup>, [Bryan Catanzaro](http://catanzaro.name/)<sup>1</sup>  
+ <sup>1</sup>NVIDIA Corporation, <sup>2</sup>UC Berkeley  
+ In CVPR 2018.  
+
+## Image-to-image translation at 2k/1k resolution
+- Our label-to-streetview results
+<p align='center'>  
+  <img src='imgs/teaser_label.png' width='400'/>
+  <img src='imgs/teaser_ours.jpg' width='400'/>
+</p>
+- Interactive editing results
+<p align='center'>  
+  <img src='imgs/teaser_style.gif' width='400'/>
+  <img src='imgs/teaser_label.gif' width='400'/>
+</p>
+- Additional streetview results
+<p align='center'>
+  <img src='imgs/cityscapes_1.jpg' width='400'/>
+  <img src='imgs/cityscapes_2.jpg' width='400'/>
+</p>
+<p align='center'>
+  <img src='imgs/cityscapes_3.jpg' width='400'/>
+  <img src='imgs/cityscapes_4.jpg' width='400'/>
+</p>
+
+- Label-to-face and interactive editing results
+<p align='center'>
+  <img src='imgs/face1_1.jpg' width='250'/>
+  <img src='imgs/face1_2.jpg' width='250'/>
+  <img src='imgs/face1_3.jpg' width='250'/>
+</p>
+<p align='center'>
+  <img src='imgs/face2_1.jpg' width='250'/>
+  <img src='imgs/face2_2.jpg' width='250'/>
+  <img src='imgs/face2_3.jpg' width='250'/>
+</p>
+
+- Our editing interface
+<p align='center'>
+  <img src='imgs/city_short.gif' width='330'/>
+  <img src='imgs/face_short.gif' width='450'/>
+</p>
+
+## Prerequisites
+- Linux or macOS
+- Python 2 or 3
+- NVIDIA GPU (11G memory or larger) + CUDA cuDNN
+
+## Getting Started
+### Installation
+- Install PyTorch and dependencies from http://pytorch.org
+- Install python libraries [dominate](https://github.com/Knio/dominate).
+```bash
+pip install dominate
 ```
-repo_name=Insight_Project_Framework # URL of your new repository
-username=mrubash1 # Username for your personal github account
-git clone https://github.com/$username/$repo_name
-cd $repo_name
-echo "export $repo_name=${PWD}" >> ~/.bash_profile
-echo "export PYTHONPATH=$repo_name/src:${PYTHONPATH}" >> ~/.bash_profile
-source ~/.bash_profile
-```
-Create new development branch and switch onto it
-```
-branch_name=dev-readme_requisites-20180905 # Name of development branch, of the form 'dev-feature_name-date_of_creation'}}
-git checkout -b $branch_name
-```
-
-## Initial Commit
-Lets start with a blank slate: remove `.git` and re initialize the repo
-```
-cd $repo_name
-rm -rf .git   
-git init   
-git status
-```  
-You'll see a list of file, these are files that git doesn't recognize. At this point, feel free to change the directory names to match your project. i.e. change the parent directory Insight_Project_Framework and the project directory Insight_Project_Framework:
-Now commit these:
-```
-git add .
-git commit -m "Initial commit"
-git push origin $branch_name
-```
-
-## Requisites
-
-- List all packages and software needed to build the environment
-- This could include cloud command line tools (i.e. gsutil), package managers (i.e. conda), etc.
-
-#### Dependencies
-
-- [Streamlit](streamlit.io)
-
-#### Installation
-To install the package above, pleae run:
-```shell
-pip install -r requiremnts
-```
-
-## Build Environment
-- Include instructions of how to launch scripts in the build subfolder
-- Build scripts can include shell scripts or python setup.py files
-- The purpose of these scripts is to build a standalone environment, for running the code in this repository
-- The environment can be for local use, or for use in a cloud environment
-- If using for a cloud environment, commands could include CLI tools from a cloud provider (i.e. gsutil from Google Cloud Platform)
-```
-# Example
-
-# Step 1
-# Step 2
+- Clone this repo:
+```bash
+git clone https://github.com/NVIDIA/pix2pixHD
+cd pix2pixHD
 ```
 
-## Configs
-- We recommond using either .yaml or .txt for your config files, not .json
-- **DO NOT STORE CREDENTIALS IN THE CONFIG DIRECTORY!!**
-- If credentials are needed, use environment variables or HashiCorp's [Vault](https://www.vaultproject.io/)
 
-
-## Test
-- Include instructions for how to run all tests after the software is installed
+### Testing
+- A few example Cityscapes test images are included in the `datasets` folder.
+- Please download the pre-trained Cityscapes model from [here](https://drive.google.com/file/d/1h9SykUnuZul7J3Nbms2QGH1wa85nbN2-/view?usp=sharing) (google drive link), and put it under `./checkpoints/label2city_1024p/`
+- Test the model (`bash ./scripts/test_1024p.sh`):
+```bash
+#!./scripts/test_1024p.sh
+python test.py --name label2city_1024p --netG local --ngf 32 --resize_or_crop none
 ```
-# Example
+The test results will be saved to a html file here: `./results/label2city_1024p/test_latest/index.html`.
 
-# Step 1
-# Step 2
+More example scripts can be found in the `scripts` directory.
+
+
+### Dataset
+- We use the Cityscapes dataset. To train a model on the full dataset, please download it from the [official website](https://www.cityscapes-dataset.com/) (registration required).
+After downloading, please put it under the `datasets` folder in the same way the example images are provided.
+
+
+### Training
+- Train a model at 1024 x 512 resolution (`bash ./scripts/train_512p.sh`):
+```bash
+#!./scripts/train_512p.sh
+python train.py --name label2city_512p
+```
+- To view training results, please checkout intermediate results in `./checkpoints/label2city_512p/web/index.html`.
+If you have tensorflow installed, you can see tensorboard logs in `./checkpoints/label2city_512p/logs` by adding `--tf_log` to the training scripts.
+
+### Multi-GPU training
+- Train a model using multiple GPUs (`bash ./scripts/train_512p_multigpu.sh`):
+```bash
+#!./scripts/train_512p_multigpu.sh
+python train.py --name label2city_512p --batchSize 8 --gpu_ids 0,1,2,3,4,5,6,7
+```
+Note: this is not tested and we trained our model using single GPU only. Please use at your own discretion.
+
+### Training with Automatic Mixed Precision (AMP) for faster speed
+- To train with mixed precision support, please first install apex from: https://github.com/NVIDIA/apex
+- You can then train the model by adding `--fp16`. For example,
+```bash
+#!./scripts/train_512p_fp16.sh
+python -m torch.distributed.launch train.py --name label2city_512p --fp16
+```
+In our test case, it trains about 80% faster with AMP on a Volta machine.
+
+### Training at full resolution
+- To train the images at full resolution (2048 x 1024) requires a GPU with 24G memory (`bash ./scripts/train_1024p_24G.sh`), or 16G memory if using mixed precision (AMP).
+- If only GPUs with 12G memory are available, please use the 12G script (`bash ./scripts/train_1024p_12G.sh`), which will crop the images during training. Performance is not guaranteed using this script.
+
+### Training with your own dataset
+- If you want to train with your own dataset, please generate label maps which are one-channel whose pixel values correspond to the object labels (i.e. 0,1,...,N-1, where N is the number of labels). This is because we need to generate one-hot vectors from the label maps. Please also specity `--label_nc N` during both training and testing.
+- If your input is not a label map, please just specify `--label_nc 0` which will directly use the RGB colors as input. The folders should then be named `train_A`, `train_B` instead of `train_label`, `train_img`, where the goal is to translate images from A to B.
+- If you don't have instance maps or don't want to use them, please specify `--no_instance`.
+- The default setting for preprocessing is `scale_width`, which will scale the width of all training images to `opt.loadSize` (1024) while keeping the aspect ratio. If you want a different setting, please change it by using the `--resize_or_crop` option. For example, `scale_width_and_crop` first resizes the image to have width `opt.loadSize` and then does random cropping of size `(opt.fineSize, opt.fineSize)`. `crop` skips the resizing step and only performs random cropping. If you don't want any preprocessing, please specify `none`, which will do nothing other than making sure the image is divisible by 32.
+
+## More Training/Test Details
+- Flags: see `options/train_options.py` and `options/base_options.py` for all the training flags; see `options/test_options.py` and `options/base_options.py` for all the test flags.
+- Instance map: we take in both label maps and instance maps as input. If you don't want to use instance maps, please specify the flag `--no_instance`.
+
+
+## Citation
+
+If you find this useful for your research, please use the following.
+
+```
+@inproceedings{wang2018pix2pixHD,
+  title={High-Resolution Image Synthesis and Semantic Manipulation with Conditional GANs},
+  author={Ting-Chun Wang and Ming-Yu Liu and Jun-Yan Zhu and Andrew Tao and Jan Kautz and Bryan Catanzaro},  
+  booktitle={Proceedings of the IEEE Conference on Computer Vision and Pattern Recognition},
+  year={2018}
+}
 ```
 
-## Run Inference
-- Include instructions on how to run inference
-- i.e. image classification on a single image for a CNN deep learning project
-```
-# Example
-
-# Step 1
-# Step 2
-```
-
-## Build Model
-- Include instructions of how to build the model
-- This can be done either locally or on the cloud
-```
-# Example
-
-# Step 1
-# Step 2
-```
-
-## Serve Model
-- Include instructions of how to set up a REST or RPC endpoint
-- This is for running remote inference via a custom model
-```
-# Example
-
-# Step 1
-# Step 2
-```
-
-## Analysis
-- Include some form of EDA (exploratory data analysis)
-- And/or include benchmarking of the model and results
-```
-# Example
-
-# Step 1
-# Step 2
-```
+## Acknowledgments
+This code borrows heavily from [pytorch-CycleGAN-and-pix2pix](https://github.com/junyanz/pytorch-CycleGAN-and-pix2pix).
---
+++ personal_scripts/preprocess.py
@@ -0,0 +1,336 @@
+import numpy as np
+import os, sys, cv2, shutil, argparse, time, concurrent.futures, imageio
+import warnings
+import itertools as it
+from PIL import Image
+from random import shuffle
+from math import floor
+from skimage import data
+import imgaug as ia
+import imgaug.augmenters as iaa
+from imgaug.augmentables.batches import Batch
+
+def quad_as_rect(quad):
+    if quad[0] != quad[2]: return False
+    if quad[0] != quad[2]: return False
+    if quad[1] != quad[7]: return False
+    if quad[4] != quad[6]: return False
+    if quad[3] != quad[5]: return False
+    return True
+
+def quad_to_rect(quad):
+    assert(len(quad) == 8)
+    assert(quad_as_rect(quad))
+    return (quad[0], quad[1], quad[4], quad[3])
+
+def rect_to_quad(rect):
+    assert(len(rect) == 4)
+    return (rect[0], rect[1], rect[0], rect[3], rect[2], rect[3], rect[2], rect[1])
+
+def shape_to_rect(shape):
+    assert(len(shape) == 2)
+    return (0, 0, shape[0], shape[1])
+
+def griddify(rect, w_div, h_div):
+    w = rect[2] - rect[0]
+    h = rect[3] - rect[1]
+    x_step = w / float(w_div)
+    y_step = h / float(h_div)
+    y = rect[1]
+    grid_vertex_matrix = []
+    for _ in range(h_div + 1):
+        grid_vertex_matrix.append([])
+        x = rect[0]
+        for _ in range(w_div + 1):
+            grid_vertex_matrix[-1].append([int(x), int(y)])
+            x += x_step
+        y += y_step
+    grid = np.array(grid_vertex_matrix)
+    return grid
+
+def distort_grid(org_grid, max_shift):
+    new_grid = np.copy(org_grid)
+    x_min = np.min(new_grid[:, :, 0])
+    y_min = np.min(new_grid[:, :, 1])
+    x_max = np.max(new_grid[:, :, 0])
+    y_max = np.max(new_grid[:, :, 1])
+    new_grid += np.random.randint(- max_shift, max_shift + 1, new_grid.shape)
+    new_grid[:, :, 0] = np.maximum(x_min, new_grid[:, :, 0])
+    new_grid[:, :, 1] = np.maximum(y_min, new_grid[:, :, 1])
+    new_grid[:, :, 0] = np.minimum(x_max, new_grid[:, :, 0])
+    new_grid[:, :, 1] = np.minimum(y_max, new_grid[:, :, 1])
+    return new_grid
+
+def grid_to_mesh(src_grid, dst_grid):
+    assert(src_grid.shape == dst_grid.shape)
+    mesh = []
+    for i in range(src_grid.shape[0] - 1):
+        for j in range(src_grid.shape[1] - 1):
+            src_quad = [src_grid[i    , j    , 0], src_grid[i    , j    , 1],
+                    src_grid[i + 1, j    , 0], src_grid[i + 1, j    , 1],
+                    src_grid[i + 1, j + 1, 0], src_grid[i + 1, j + 1, 1],
+                    src_grid[i    , j + 1, 0], src_grid[i    , j + 1, 1]]
+            dst_quad = [dst_grid[i    , j    , 0], dst_grid[i    , j    , 1],
+                    dst_grid[i + 1, j    , 0], dst_grid[i + 1, j    , 1],
+                    dst_grid[i + 1, j + 1, 0], dst_grid[i + 1, j + 1, 1],
+                    dst_grid[i    , j + 1, 0], dst_grid[i    , j + 1, 1]]
+            dst_rect = quad_to_rect(dst_quad)
+            mesh.append([dst_rect, src_quad])
+    return mesh
+
+def resize(args):
+    img_size,filepath = args
+    sq_img = cv2.imread(filepath) # square image
+    scaled_sq_img = resizeAndPad(sq_img, (img_size,img_size), 127)
+    cv2.imwrite(filepath, scaled_sq_img)
+
+def resizeAndPad(img, size, padColor=0):
+
+    h, w = img.shape[:2]
+    sh, sw = size
+
+    # interpolation method
+    if h > sh or w > sw: # shrinking image
+        interp = cv2.INTER_AREA
+    else: # stretching image
+        interp = cv2.INTER_CUBIC
+    # aspect ratio of image
+    aspect = w/h  # if on Python 2, you might need to cast as a float: float(w)/h
+
+    # compute scaling and pad sizing
+    if aspect > 1: # horizontal image
+        new_w = sw
+        new_h = np.round(new_w/aspect).astype(int)
+        pad_vert = (sh-new_h)/2
+        pad_top, pad_bot = np.floor(pad_vert).astype(int), np.ceil(pad_vert).astype(int)
+        pad_left, pad_right = 0, 0
+    elif aspect < 1: # vertical image
+        new_h = sh
+        new_w = np.round(new_h*aspect).astype(int)
+        pad_horz = (sw-new_w)/2
+        pad_left, pad_right = np.floor(pad_horz).astype(int), np.ceil(pad_horz).astype(int)
+        pad_top, pad_bot = 0, 0
+    else: # square image
+        new_h, new_w = sh, sw
+        pad_left, pad_right, pad_top, pad_bot = 0, 0, 0, 0
+
+    # set pad color
+    if len(img.shape) is 3 and not isinstance(padColor, (list, tuple, np.ndarray)): # color image but only one color provided
+        padColor = [padColor]*3
+
+    # scale and pad
+    scaled_img = cv2.resize(img, (new_w, new_h), interpolation=interp)
+    scaled_img = cv2.copyMakeBorder(scaled_img, pad_top, pad_bot, pad_left, pad_right, borderType=cv2.BORDER_CONSTANT, value=padColor)
+
+    return scaled_img
+
+def get_file_list_from_dir(datadir):
+    all_files = os.listdir(os.path.relpath(datadir))
+    data_files = list(filter(lambda file: file.endswith('.png'), all_files))
+    return data_files
+
+def randomize_files(file_list):
+    shuffle(file_list)
+    return file_list
+
+def get_training_and_testing_sets(train, file_list):
+    #Initial split for training and test data
+    split = train
+    split_index = floor(len(file_list) * split)
+    training = file_list[:split_index]
+    final_testing = file_list[split_index:]
+    #Secondary split for validation
+    split_index = floor(len(training) * split)
+    final_training = training[:split_index]
+    return final_training, final_testing
+
+def warp(args):
+    filename, root, fold_A = args
+    im = Image.open(os.path.join(root,filename))
+    dst_grid = griddify(shape_to_rect(im.size), 4, 4)
+    src_grid = distort_grid(dst_grid, 50)
+    mesh = grid_to_mesh(src_grid, dst_grid)
+    im = im.transform(im.size, Image.MESH, mesh)
+    im.save(os.path.join(fold_A,root.rsplit('/', 1)[-1],filename))
+
+def png2jpg(args):
+    filepath = args
+    im = Image.open(filepath)
+    im = im.convert('RGB')
+    im.save(os.path.splitext(filepath)[0] + '.jpg', quality=100)
+
+
+def rotate_image(mat, angle):
+
+    height, width = mat.shape[:2] # image shape has 3 dimensions
+    image_center = (width/2, height/2) # getRotationMatrix2D needs coordinates in reverse order (width, height) compared to shape
+
+    rotation_mat = cv2.getRotationMatrix2D(image_center, angle, 1.)
+
+    abs_cos = abs(rotation_mat[0,0]) 
+    abs_sin = abs(rotation_mat[0,1])
+
+    bound_w = int(height * abs_sin + width * abs_cos)
+    bound_h = int(height * abs_cos + width * abs_sin)
+
+    rotation_mat[0, 2] += bound_w/2 - image_center[0]
+    rotation_mat[1, 2] += bound_h/2 - image_center[1]
+
+    rotated_mat = cv2.warpAffine(mat, rotation_mat, (bound_w, bound_h))
+    return rotated_mat
+
+
+def imgaug(args):
+    # Number of batches and batch size for this example
+    filename, root, fold_A = args
+    img = cv2.imread(os.path.join(root,filename))
+    print('image opened ' + os.path.join(root,filename))
+    batch_size = 4
+    for i in range(0,batch_size):
+        imageio.imwrite(os.path.join(root, os.path.splitext(filename)[0] + '_' + str(i) + '.jpg'), img) #convert the current image in B into a jpg from png
+    nb_batches = 1
+
+    # Example augmentation sequence to run in the background
+    sometimes = lambda aug: iaa.Sometimes(0.4, aug)
+    augseq = iaa.Sequential(
+            [
+                iaa.PiecewiseAffine(scale=(0.01, 0.01005))
+            ]
+        )
+
+    # Make batches out of the example image (here: 10 batches, each 32 times
+    # the example image)
+    batches = []
+    for _ in range(nb_batches):
+        batches.append(Batch(images=[img] * batch_size))
+
+    #Save images
+    for batch in augseq.augment_batches(batches, background=False):
+        count = 0
+        for img in batch.images_aug:
+            path = os.path.join(fold_A,root.rsplit('/', 1)[-1], os.path.splitext(filename)[0] + '_' + str(count) + '.jpg') 
+            cv2.imwrite(path, img)
+            print('image saved as: ' + path)
+            count +=1
+
+def resize_and_rotate(filename):
+    img = cv2.imread(filename)[:, :, ::-1]
+    print('image opened: ' + filename)
+    img = rotate_image(img,90)
+    img = ia.imresize_single_image(img, (1024, 2048))
+
+    cv2.imwrite(filename, img)
+    print('image rotated and resized saved as: ' + filename)
+
+
+
+def upsample(args):
+    filename, root = args
+    img = imageio.imread(os.path.join(root, filename))
+    img = rotate_image(img,-90)
+    img = ia.imresize_single_image(img, (4400, 3400))
+    cv2.imwrite(os.path.join(root, filename))
+
+
+def main():
+    #Note: Atm, it is necessary to have a 'train', 'val', and 'test' folder under folder A and B already before running the script
+    #arg parsing
+    parser = argparse.ArgumentParser('Completely preprocess all data for pix2pix')
+    parser.add_argument('--raw_data', dest='raw_data', help='input directory for all initial flatbed images', type=str, default='./raw_data')
+    parser.add_argument('--dest_dir', dest='dest_dir', help='destination directory for processed images', type=str, default='./datasets')
+    parser.add_argument('--train', dest='train', help='% of data that are training (this will also determine validation split)', type=float, default=0.7)
+    parser.add_argument('--imgsize', dest='imgsize', help='# of pixels (in an n pixels x n pixels square) that you want images to be resized to', type=int, default=512)
+    parser.add_argument('--split', dest='split', help='determine if you want to split or not', type=bool, default=False)
+    parser.add_argument('--resize', dest='resize', help='to resize a set of images', type=bool, default=False)
+    parser.add_argument('--resize_fold', dest='resize_fold', help='the folder where you want jpgs to be resized', type=str, default='./')
+    parser.add_argument('--preprocess', dest='preprocess', help='complete preprocessing', type=bool, default=False)
+    parser.add_argument('--removepng', dest='removepng', help='removes pngs', type=bool, default=False)
+    parser.add_argument('--png2jpg', dest='png2jpg', help='convert folder of choice to jpg', type=str, default="")
+    
+
+    args = parser.parse_args()
+    warnings.simplefilter(action='ignore', category=FutureWarning)
+    #print args
+    for arg in vars(args):
+        print('[%s] = ' % arg, getattr(args, arg))
+
+    start = time.clock()
+    total = time.clock()
+
+    #1. Begin splitting data
+    if(args.split == True):
+        print('Splitting data...')
+        datadir = args.raw_data
+        data_files = get_file_list_from_dir(datadir)
+        randomized = randomize_files(data_files)
+        training, testing = get_training_and_testing_sets(args.train, randomized)
+
+        train_A_dir = os.path.join(args.dest_dir,'train_A')
+        train_B_dir = os.path.join(args.dest_dir,'train_B'))
+        test_A_dir = os.path.join(args.dest_dir,'test_A'))
+        test_B_dir = os.path.join(args.dest_dir,'test_B'))
+        
+        os.mkdir(train_A_dir)
+        os.mkdir(train_B_dir)
+        os.mkdir(test_A_dir)
+        os.mkdir(test_B_dir)
+
+        count = 0
+        for f in training:
+            shutil.copy(args.raw_data + f, os.path.join( train_B_dir, "IMG_" + str(count) + ".png")
+            count += 1
+        for f in testing:
+            shutil.copy(args.raw_data + f, os.path.join( test_B_dir, "IMG_" + str(count) + ".png")
+            count +=1
+        print('Splitting took {:.3f} seconds'.format((time.clock() - start)*1000.0))
+
+    if(args.preprocess == True):
+        #2. Perform image warping and save those image into folder A
+        print('Resizing, Warping, and Converting images...')
+        total2 = time.clock()
+
+        #imgaug
+        start = time.clock()
+        with concurrent.futures.ProcessPoolExecutor() as executor:
+            for root, dirs, files in os.walk():
+                conv_list = ((file, root, args.fold_A) for file in files)
+                executor.map(imgaug, conv_list)
+        executor.shutdown(wait=True)
+        print('Augmentation took {:.3f} seconds'.format((time.clock() - start)*1000.0))
+    
+    if(args.png2jpg != ""):
+        print("Converting all pngs to jpgs")
+        with concurrent.futures.ProcessPoolExecutor() as executor:
+            for root, dirs, files in os.walk(args.png2jpg):
+                conv_list = (os.path.join(root,file) for file in files)
+                executor.map(png2jpg, conv_list)
+        executor.shutdown(wait=True) 
+        print("Conversion to jpgs done")
+
+    if(args.resize == True):
+        print('Resizing data')
+        start = time.clock()
+        with concurrent.futures.ProcessPoolExecutor() as executor:
+            process_list = []
+            for filename in os.listdir(args.resize_fold):
+                if filename.endswith(".jpg"):
+                    process_list.append(args.resize_fold + filename)
+            executor.map(resize_and_rotate, process_list) 
+        executor.shutdown(wait=True)
+        print('Rotations took {:.3f} seconds'.format((time.clock() - start)*1000.0))
+    
+    if(args.removepng == True):
+        #3. Removal of pngs
+        print("Removing png's")
+        for root, dirs, files in os.walk(args.fold_A):
+            for file in files:
+                if file.endswith('.png'):
+                    os.remove(os.path.join(root,file))
+        for root, dirs, files in os.walk(args.fold_B):
+            for file in files:
+                if file.endswith('.png'):
+                    os.remove(os.path.join(root,file))
+        print("Png's removed")
+   
+if __name__ == "__main__":
+    main()
